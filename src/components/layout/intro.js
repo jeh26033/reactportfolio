4a5cae0f5c4740331bae401eb70627ea4970840c
joseph <jeh26033@email.vccs.edu>
2020-08-18 16:09:27 -0400

    Intro style re-work
    did lots of work to the main intro page

--- src/components/layout/intro.js
+++ src/components/layout/intro.js
@@ -3,16 +3,21 @@ import './CSS/intro.css';
 
 
 function Intro() {
-	return (
-		<div className="intro">
-        	<h1 className= "largeText"><span className ="introRule">.joseph </span>{` {`}</h1>
-        	<div className="introSubtext">
-	        	<p className="notAsLargeText"><span className="introRule">web-developer:</span> 100%;</p>
-	        	<p className="notAsLargeText"><span className="introRule">front-end-developer:</span> bold;<br/><span className="introRule"> web-designer:</span> true; <br/><span className="introRule">freelancer:</span> bolder;<br/><span className="introRule">hot-pocket-connoisseur:</span> absolute;</p>
-        	</div>
-        	<h1 className= "largeText">{`}`}</h1>
-	    </div>
-	)
+    return (
+    	<div className="intro">
+	        <div className="introBG">
+
+		    </div>
+		    <div className="introContent">
+		        	<h1 className= "largeText"><span className ="introRule">.joseph </span>{` {`}</h1>
+		        	<div className="introSubtext">
+			        	<p className="notAsLargeText"><span className="introRule">web-developer:</span> 100%;</p>
+			        	<p className="notAsLargeText"><span className="introRule">front-end-developer:</span> bold;<br/><span className="introRule"> web-designer:</span> true; <br/><span className="introRule">freelancer:</span> bolder;<br/><span className="introRule">hot-pocket-connoisseur:</span> absolute;</p>
+		        	</div>
+		        	<h1 className= "largeText">{`}`}</h1>
+		        </div>
+		 </div>
+    )
 }
 
 
--- src/components/layout/mobileIntro.js
+++ src/components/layout/mobileIntro.js
@@ -11,10 +11,6 @@ function MobileIntro() {
 			        	<p className="notAsLargeText"><span className="introRule">web-developer:</span> 100%;</p>
 			        	<p className="notAsLargeText"><span className="introRule">front-end-developer:</span> bold;<br/><span className="introRule"> web-designer:</span> true; <br/><span className="introRule">freelancer:</span> bolder;<br/><span className="introRule">hot-pocket-connoisseur:</span> absolute;</p>
 		        	</div>
-        	
-
-        	
-
         	<h1 className= "largeText">{`}`}</h1>
 	  </div>
         </React.Fragment>
